ee1129321e7cb65f21c98172b41da582eeb7f6f0
ThOpaque_MBA <thierry.kss@posteo.net>
2023-01-11 21:32:13 +0100

    Separate the train and val dataset

--- main_project/FaceVerification/siamese_dataset_2faces.py
+++ main_project/FaceVerification/siamese_dataset_2faces.py
@@ -12,23 +12,17 @@ import torchvision
 
 class SiameseDataset(torch.utils.data.Dataset):
     def __init__(self, isValSet_bool:bool=None, isAugment_bool:bool=False, isNormalize_bool:bool=False)->tuple: 
-        imgset_self = glob.glob('/Users/thierryksstentini/Downloads/dataset/dataset_sven/dataset_moi_sven_cropped/*')
-        imgset_lfw = glob.glob('/Users/thierryksstentini/Downloads/dataset/dataset_sven/dataset_pauline_sven_cropped/*')
-        len_self = len(imgset_self)
-        len_lfw = len(imgset_lfw)
-
+        dataset_type = 'train'
         if isValSet_bool:
-            imgset_lfw = rd.sample(imgset_lfw, int(len_lfw*0.3)) 
-            imgset_self = rd.sample(imgset_self, int(len_self*0.3))
-        else :
-            imgset_lfw = rd.sample(imgset_lfw, int(len_lfw*0.7)) 
-            imgset_self = rd.sample(imgset_self, int(len_self*0.7))
+            dataset_type = 'val'
+
+        imgset_self = glob.glob(f'/Users/thierryksstentini/Downloads/dataset/dataset_sven/dataset_moi_sven_cropped/{dataset_type}/*')
+        imgset_lfw = glob.glob(f'/Users/thierryksstentini/Downloads/dataset/dataset_sven/dataset_pauline_sven_cropped/{dataset_type}/*')
+        self.imgset = imgset_lfw + imgset_self
         
         label_lfw = np.zeros(len(imgset_lfw)).tolist()
         label_self = np.ones(len(imgset_self)).tolist()
-        
         self.labelset = label_lfw + label_self
-        self.imgset = imgset_lfw + imgset_self
 
         self.isNormalize_bool = isNormalize_bool
         self.isAugment_bool = isAugment_bool
@@ -43,21 +37,21 @@ class SiameseDataset(torch.utils.data.Dataset):
 
         ### Normalize data
         if self.isNormalize_bool:
-            mean, std = (0.3533, 0.3867, 0.5007), (0.2228, 0.2410, 0.2774)
+            mean, std = (0.3510, 0.3846, 0.4988), (0.2214, 0.2394, 0.2772)
             img_t = torchvision.transforms.Normalize(mean, std)(img_t)
-        return img_t
 
         ### Data augmentation
         if self.isAugment_bool:
             augment = torchvision.transforms.Compose([
                 torchvision.transforms.RandomHorizontalFlip(p=0.5),
-                torchvision.transforms.ColorJitter(brightness=[0.5,1], contrast=[1,1.2], saturation=[0,1]),
-                torchvision.transforms.RandomAdjustSharpness(12, p=0.7),
+                torchvision.transforms.ColorJitter(brightness=[0.6,1], contrast=[1,1.2], saturation=[0,1]),
+                torchvision.transforms.RandomAdjustSharpness(12, p=0.5),
                 torchvision.transforms.RandomRotation(degrees=(-10,10)),
                 torchvision.transforms.RandomPerspective(distortion_scale=0.22, p=0.5, fill=0),
                 torchvision.transforms.RandomResizedCrop(size=(100, 100), scale=(0.85, 0.85))
              ])
             img_t = augment(img_t)
+        return img_t
 
     def __len__(self):
         return len(self.imgset)
@@ -72,7 +66,7 @@ class SiameseDataset(torch.utils.data.Dataset):
         img_PIL = PIL.Image.open(img_path).convert('RGB')
         image = self._preprocess(img_PIL)
 
-        return image, torch.tensor(label, dtype=torch.int32)
+        return image, torch.tensor(label).to(torch.int32)
 
 
 def get_training_dataset(BATCH_SIZE=16, **kwargs):
@@ -95,5 +89,5 @@ def get_validation_dataset(BATCH_SIZE=None, **kwargs):
 
 
 if __name__ == "__main__":
-    dataset = get_training_dataset(ratio=1, isAugment_bool=True, isNormalize_bool=False)
+    dataset = get_validation_dataset(isAugment_bool=False, isNormalize_bool=True)
     print(next(iter(dataset))[1])
